dacc7bd361e9b0e66aaa0899508cd1323a1608ea
Janine Kostka <janine.kostka@gmail.com>
2017-04-16 15:42:25 +0200

    improve responsiveness

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage();
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Home', 'About me', 'Online CV'],
+        continuousVertical: true,
+        keyboardScrolling: true,
+
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+        menu: '#myMenu'
+    });
 });
